1a35a9648e9f6d88867bf8295e7ec4e7058af703
Avinashdeshan <avinashdeshan11@gmail.com>
2025-02-19 03:38:02 +0530

    update dashboard

--- src/main/java/Controller/dashboard/DashboardFormController.java
+++ src/main/java/Controller/dashboard/DashboardFormController.java
@@ -3,8 +3,11 @@ package Controller.dashboard;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -17,39 +20,46 @@ public class DashboardFormController {
         URL resourses=this.getClass().getResource("/view/employee.fxml");
         assert resourses !=null;
 
-        Parent load= FXMLLoader.load(resourses);
+        Parent load= FXMLLoader.<Parent>load(resourses);
         this.loadDashboard.getChildren().clear();
         this.loadDashboard.getChildren().add(load);
     }
 
     @FXML
     void btnAddSupplierOnAction(ActionEvent event) throws IOException {
-        URL resourses=this.getClass().getResource("/view/customermanagement.fxml");
+        URL resourses=this.getClass().getResource("/view/supplier.fxml");
         assert resourses !=null;
 
-        Parent load= FXMLLoader.load(resourses);
+        Parent load= FXMLLoader.<Parent>load(resourses);
         this.loadDashboard.getChildren().clear();
         this.loadDashboard.getChildren().add(load);
     }
 
     @FXML
     void btnHomeOnAction(ActionEvent event) throws IOException {
-        URL resourses=this.getClass().getResource("/view/home.fxml");
+        URL resourses=this.getClass().getResource("/view/admindashboardHome.fxml");
         assert resourses !=null;
 
-        Parent load= FXMLLoader.load(resourses);
+        Parent load= FXMLLoader.<Parent>load(resourses);
         this.loadDashboard.getChildren().clear();
         this.loadDashboard.getChildren().add(load);
     }
 
     @FXML
     void btnLogoutOnAction(ActionEvent event) throws IOException {
-        URL resourses=this.getClass().getResource("/view/home.fxml");
-        assert resourses !=null;
+        URL resource = getClass().getResource("/view/home.fxml");
+        assert resource != null;
 
-        Parent load= FXMLLoader.load(resourses);
-        this.loadDashboard.getChildren().clear();
-        this.loadDashboard.getChildren().add(load);
+        Parent root = FXMLLoader.load(resource);
+        Scene scene = new Scene(root);
+
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.close();
+
+        Stage homeStage = new Stage();
+        homeStage.setScene(scene);
+        homeStage.setTitle("Home");
+        homeStage.show();
     }
 
     @FXML
